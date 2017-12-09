302528146d58a977cae56ad60193489d6d676f63
Pranav Kushwaha <30798744+TheBossBaby@users.noreply.github.com>
2017-12-09 22:06:18 +0530

    This C++ programe converts the selected currency to US Dollar.
    In this programe I have used switch-case.

--- Chapter III/Try This/Currency Converter/Currency Converter/Source.cpp
+++ Chapter III/Try This/Currency Converter/Currency Converter/Source.cpp
@@ -6,30 +6,41 @@ int main()
 {
 	//Conversion rates of currency to USD
 	const double euro_to_usd{1.18};
-	const double yen_to_usd{ 0.0088 };
 	const double pound_to_usd{ 1.34 };
+	const double yuan_to_usd{ 0.15 };
+	const double kroner_to_usd{ 0.16 };
 	int ammount{ 0 };
-	string currency{ "???" };
+	char currency{ '?' };
 
 	cout << "\tThis C++ Programe converts the ammount in currency selected by you to US Dollar:\n "
 		<< "Please Select your currnecy:\n"
-		<< "y = Yen\n"
 		<< "e = Euro\n"
-		<< "p = Pounds\n";
+		<< "p = Pounds\n"
+		<< "y = Yuan\n"
+		<< "k = Kroner\n";
 	cin >> currency;
 	cout << "Enter the ammount: ";
 	cin >> ammount;
 
 	//Compare the currency and convert according to given currency.
 
-	if (currency == "y")
-		cout << ammount << " yen = $" << ammount * yen_to_usd<<" \n";
-	else if (currency == "e")
+	switch (currency)
+	{
+	case 'e':
 		cout << ammount << " euro = $ " << ammount * euro_to_usd << " \n";
-	else if (currency == "p")
+		break;
+	case 'p':
 		cout << ammount << " pounds = $ " << ammount * pound_to_usd << "\n";
-	else
+		break;
+	case 'y':
+		cout << ammount << " yuan = $ " << ammount * yuan_to_usd << "\n";
+		break;
+	case 'k':
+		cout << ammount << " kroner = $ " << ammount * kroner_to_usd << "\n";
+		break;
+	default:
 		cout << "\n\nSorry I can't do that conversion.\n\a";
+	}
 
 	keep_window_open();
 	return 0;
