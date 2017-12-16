88c03d2fa22ab77cef0944a437e001fb003575c6
Pranav Kushwaha <30798744+TheBossBaby@users.noreply.github.com>
2017-12-16 09:42:47 +0530

    Changes made in input.
    In this commit I took input in two variable rather then using an vector.

--- Chapter III/Drill/Project1/Project1/Source.cpp
+++ Chapter III/Drill/Project1/Project1/Source.cpp
@@ -2,33 +2,33 @@
 
 int main()
 {
-	vector <double> number; //An empty vector of double
-	double temp{ 0.0 };
+	double number1{ 0 };
+	double number2{ 0 };
 	cout << "Please enter 2 integers(To exit type '|'): ";
 
-	while (cin >> temp) //If temp is double cin>>temp is true else it is false
+	while (cin >> number1 >> number2) //If number1 & number2 are double cin>>number1>>number2 is true else it is false
 	{
-		number.push_back(temp); //Enter temp at last of vector
-		cout << "You Entered " << temp << "\n"; 
-	}
+		cout << "\nYou Entered " << number1 << " and " << number2;
 
+		//Compare number1 and nuumber2
+		if (number1 < number2) //
+		{
+			cout << "\nThe smaller value is: " << number1
+				<< "\nThe larger value is: " << number2 << "\n";
+		}
+		else if (number1 > number2)
+		{
+			cout << "\nThe smaller value is: " << number2
+				<< "\nThe larger value is: " << number1 << "\n";
+		}
+		else
+		{
+			cout << "\nBoth value are almost equal.\n";
+		}
+		
+	}
 	keep_window_open();
-	//Compare the first and second number of vector
-	if(number[0] < number[1]) //
-	{
-		cout << "\nThe smaller value is: " << number[0]
-			<< "\nThe larger value is: " << number[1] << "\n";
-	}
-	else if (number[0] > number[1])
-	{
-		cout << "\nThe smaller value is: " << number[1]
-			<< "\nThe larger value is: " << number[0]<<"\n";
-	}
-	else if(number[0] - number[1] < 1.0/100 || number[1] - number[0] < 1.0/100) 
-	{
-		cout << "\nBoth value are almost equal.\n";
-	}
-	
+
 	keep_window_open();
 	return 0;
 }
