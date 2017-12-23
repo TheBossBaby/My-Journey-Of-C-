8fb2441a9a6ea09ec431933fbc8dc4f03dfa7eaf
Pranav Kushwaha <30798744+TheBossBaby@users.noreply.github.com>
2017-12-23 14:27:15 +0530

    Slight change in code design
    I divided the code in module

--- Chapter III/Exercise/DigitToSpellingConverter/DigitToSpellingConverter/Source.cpp
+++ Chapter III/Exercise/DigitToSpellingConverter/DigitToSpellingConverter/Source.cpp
@@ -3,39 +3,57 @@
 
 #include"std_lib_facilities.h"
 
-int main()
+//This function takes a digit and convert it to its spelling
+string digit_to_spelling(int input_digit, vector <string> spelled_digit)
 {
-	vector <string> spelled_digit{ "zero", "one","two","three","four","five","six","seven","eight","nine" };
-	int input_digit{ -1 };
-	cout << "\n\tEnter the digit from(0-9): ";
-	cin >> input_digit;
-	string input_spelling{ "??" };
-	cout << "\n\tEnter the spelling(in lower case) form (zero-nine): ";
-	cin >> input_spelling;
-
 	int i{ -1 }; //Used for iterating
-	if (input_digit > spelled_digit.size())
-		cout << "\nInput out of range!!\a\a\n";
-	else
+	for (string x : spelled_digit)
 	{
-		for (string x : spelled_digit)
-		{
-			i++; //It stores the digit 
-			if (i == input_digit)
-				cout << "\n\t" << input_digit << " is spelled as: " << x << "\n";
-		}
+		i++; //It stores the digit 
+		if (i == input_digit)
+			return x;
 	}
 
-	i = -1;//Reset the iterator
+	simple_error("Input is out of range!!!!");
+}
+
+//This function takes spelling of digit and prints the digit in numeric form
+int spelling_to_digit(string input_spelling, vector <string> spelled_digit)
+{
+	int i{ -1 };
 	for (string x : spelled_digit)
 	{
 		i++;
 		if (x == input_spelling)
-			cout << "\n\t" <<x << " is spelling of digit: " << i << "\n";
+			return i; //Returns the digit
 	}
 
-	if (i > input_spelling.size())
-		cout << input_spelling << " is out of range!!!!!\n\n\a";
+	//If the for statement does not return anything it means the input is out of range!!!
+		simple_error(" is out of range!!!!!\n\n\a");
+}
+
+int main()
+{
+	vector <string> spelled_digit{ "zero", "one","two","three","four","five","six","seven","eight","nine" };
+	int input_digit{ -1 };
+
+	//Digit to spelling
+	cout << "\n\tEnter the digit from(0-9): ";
+	cin >> input_digit;
+	if (input_digit > spelled_digit.size())
+		cout << "\nInput out of range!!\a\a\n";
+	else
+	{
+		cout << "\n\t" << input_digit << " is spelled as: " << digit_to_spelling(input_digit, spelled_digit) << "\n";
+	}
+
+	string input_spelling{ "??" };
+	cout << "\n\tEnter the spelling(in lower case) form (zero-nine): ";
+	cin >> input_spelling;
+	
+	//Spelling to digit
+	cout << "\n\t"<<input_spelling<<" is the spelling of the digit: "<<spelling_to_digit(input_spelling, spelled_digit)<<"\n";
+	
 	keep_window_open();
 	return 0;
 }
